a1e8948443d80f57d5e4f1e072a4be25a6c2ac81
Michael Lyon <lyoncodes@gmail.com>
2020-03-05 21:57:09 -0800

    adds store, files, state data

---
+++ docs/src/data/SEOcards.js
@@ -0,0 +1,22 @@
+export default [
+// Add new cards here
+  {
+    title: 'SEO Card',
+    text: 'test1 text',
+    href: '#',
+    id: 1
+  },
+  {
+    title: 'SEO Card',
+    text: 'test2 text',
+    href: '#',
+    id: 2
+  },
+  {
+    title: 'SEO Card',
+    text: 'test3 text',
+    href: '#',
+    id: 3
+  }
+
+]
---
+++ docs/src/data/SGcards.js
@@ -0,0 +1,22 @@
+export default [
+// Add new cards here
+  {
+    title: 'Style Guide',
+    text: 'test1 text',
+    href: '#',
+    id: 'seo-1'
+  },
+  {
+    title: 'Style Guide',
+    text: 'test2 text',
+    href: '#',
+    id: 'seo-2'
+  },
+  {
+    title: 'Style Guide',
+    text: 'test3 text',
+    href: '#',
+    id: 'seo-3'
+  }
+
+]
