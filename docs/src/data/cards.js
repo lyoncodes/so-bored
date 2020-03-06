958f8b96fd2b31c3c2b04aa51724f2b3364c4cb4
Michael Lyon <lyoncodes@gmail.com>
2020-03-05 16:16:09 -0800

    commits development branch

---
+++ docs/src/data/cards.js
@@ -0,0 +1,19 @@
+export default [
+// Add new cards here
+  {
+    title: 'test1',
+    text: 'test1 text',
+    href: '#'
+  },
+  {
+    title: 'test2',
+    text: 'test2 text',
+    href: '#'
+  },
+  {
+    title: 'test3',
+    text: 'test3 text',
+    href: '#'
+  }
+
+]
--- docs/src/views/Seo.vue
+++ docs/src/views/Seo.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>SEO</h1>
+    <b-card-group class="card-grid">
+        <template v-for="card in cards">
+        <!-- card component goes here eventually -->
+          <b-col md="4" class="card-container" :key="card">
+            <b-card bg-variant="dark" text-variant="white">
+                <h3>
+                  {{card.title}}
+                </h3>
+                <b-card-text>
+                    {{card.text}}
+                </b-card-text>
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+        </template>
+    </b-card-group>
 </template>
+<script>
+import cards from '@/data/cards'
+export default {
+  data () {
+    return {
+      cards
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
