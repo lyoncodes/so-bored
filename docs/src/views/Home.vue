00a141666c74387c09e0258bbfdbe2c8d0d3976c
Michael Lyon <lyoncodes@gmail.com>
2020-03-29 16:07:54 -0700

    adds pug

--- docs/src/views/Home.vue
+++ docs/src/views/Home.vue
@@ -1,30 +1,29 @@
-<template>
-  <div class="home">
-    <img src="../assets/unearth-slate.svg">
+<template lang="pug">
+  .home
+    .img(src="../assets/unearth-slate.svg")
     <h1>Documents</h1>
     <button @click="toggleCardForm" class="btn btn-primary">Add New Card</button>
     <b-form @submit.prevent="handleSubmit" v-if="showCardForm">
       <b-col md="6">
         <b-form-group id="input-title" label="Card Title" label-for="input-title">
-          <b-form-input
+          .b-form-input(
             id="card-title"
             v-model="formData.title"
             required
             placeholder="Enter name"
-          ></b-form-input>
+            )
         </b-form-group>
         <b-form-group id="input-card-text" label="Card Text:" label-for="input-card-text">
-          <b-form-input
+          .b-form-input(
             id="card-text"
             v-model="formData.text"
             required
-          ></b-form-input>
+          )
         </b-form-group>
       </b-col>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
-  </div>
 </template>
 
 <script>
