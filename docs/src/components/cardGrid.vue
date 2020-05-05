353595e177969ad4d9653cdac2410301d7e127be
Michael Lyon <lyoncodes@gmail.com>
2020-05-05 09:32:14 -0700

    refactors component

---
+++ docs/src/components/cardGrid.vue
@@ -0,0 +1,121 @@
+<template lang="pug">
+  b-card-group(class="card-grid")
+    b-container(class="card-container")
+      b-row
+        b-col(md="4" v-for="card in pinnedCards" :key="card.active")
+          b-card(bg-variant="dark" text-variant="white")
+            //- card title -------
+            h3(v-if="!updateData.title.length || updateData.title.length && !card.updating") {{card.title}}
+            h3(v-if="updateData.title.length && card.updating") {{updateData.title}}
+            //-------------
+            b-form(@submit.prevent="submitUpdate(card)" v-if="card.updating")
+              b-form-textarea(
+                id="card-title"
+                v-model="updateData.title"
+                :placeholder="card.title"
+              )
+            //- card text ------
+            b-card-text(v-if="!updateData.text.length || updateData.text.length && !card.updating") {{card.text}}
+            b-card-text(v-if="updateData.text.length && card.updating") {{updateData.text}}
+            //-----------
+            b-form(@submit.prevent="submitUpdate(card)" v-if="card.updating")
+              b-form-textarea(
+                id="card-text"
+                v-model="updateData.text"
+                :placeholder="card.text"
+              )
+              b-button(type="submit" variant="primary" v-if="updateData.updating") {{templateText.updateBtn}}
+            b-button(@click="handleUpdate(card)" variant="primary" v-if="!card.updating") {{templateText.updateRule}}
+            b-button(@click="handleCancel(card)" variant="primary" v-if="updateData.updating") {{templateText.cancelBtn}}
+            b-button(@click="handleHide(card)" variant="primary" v-if="!card.updating") {{templateText.hideBtn}}
+</template>
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'menu-row',
+  data () {
+    return {
+      switchesActive: false,
+      updateData: {
+        title: '',
+        text: '',
+        updating: false
+      },
+      templateText: {
+        toggleMsg: 'Show All Switches',
+        toggleMsgAlt: 'Hide All Switches',
+        updateBtn: 'Update!',
+        updateRule: 'Update Rule',
+        cancelBtn: 'Nvm',
+        hideBtn: 'Hide'
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      'Cards',
+      'pinnedCards'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'showUpdateField',
+      'updateCard',
+      'hidePin'
+    ]),
+    handleUpdate (card) {
+      this.switchesActive = !this.switchesActive
+      this.updateData.updating = !this.updateData.updating
+      const { id, updating } = card
+      const updatePayload = {
+        id,
+        updating
+      }
+      this.showUpdateField(updatePayload)
+    },
+    submitUpdate (card) {
+      const cardId = card.id
+      const { title, text, updating } = this.updateData
+      const updateData = {
+        title,
+        text,
+        cardId,
+        updating
+      }
+      this.updateCard(updateData)
+      this.clearForm()
+      this.switchesActive = !this.switchesActive
+    },
+    handleCancel (card) {
+      this.switchesActive = !this.switchesActive
+      this.updateData.updating = !this.updateData.updating
+      const id = card.id
+      const updating = false
+      const blankPayload = {
+        id,
+        updating
+      }
+      this.showUpdateField(blankPayload)
+      this.clearForm()
+    },
+    handleHide (card) {
+      this.hidePin(card)
+    },
+    clearForm () {
+      this.updateData = {
+        title: '',
+        text: '',
+        updating: false
+      }
+    }
+  },
+  mounted () {
+    const card = this.Cards
+    this.card = card
+  }
+
+}
+</script>
+<style scoped>
+
+</style>
--- docs/src/components/menuGrid.vue
+++ docs/src/components/menuGrid.vue
@@ -15,39 +15,12 @@
           :class="{ selected: card === selectedCard }"
           @click="handleSwitch(card, idx)"
           variant="primary") {{card.title}}
-    b-card-group(class="card-grid")
-      b-container(class="card-container")
-        b-row
-          b-col(md="4" v-for="card in pinnedCards" :key="card.active")
-            b-card(bg-variant="dark" text-variant="white")
-              //- card title -------
-              h3(v-if="!updateData.title.length || updateData.title.length && !card.updating") {{card.title}}
-              h3(v-if="updateData.title.length && card.updating") {{updateData.title}}
-              //-------------
-              b-form(@submit.prevent="submitUpdate(card)" v-if="card.updating")
-                b-form-textarea(
-                  id="card-title"
-                  v-model="updateData.title"
-                  :placeholder="card.title"
-                )
-              //- card text ------
-              b-card-text(v-if="!updateData.text.length || updateData.text.length && !card.updating") {{card.text}}
-              b-card-text(v-if="updateData.text.length && card.updating") {{updateData.text}}
-              //-----------
-              b-form(@submit.prevent="submitUpdate(card)" v-if="card.updating")
-                b-form-textarea(
-                  id="card-text"
-                  v-model="updateData.text"
-                  :placeholder="card.text"
-                )
-                b-button(type="submit" variant="primary" v-if="updateData.updating") {{templateText.updateBtn}}
-              b-button(@click="handleUpdate(card)" variant="primary" v-if="!card.updating") {{templateText.updateRule}}
-              b-button(@click="handleCancel(card)" variant="primary" v-if="updateData.updating") {{templateText.cancelBtn}}
-              b-button(@click="handleHide(card)" variant="primary" v-if="!card.updating") {{templateText.hideBtn}}
+    cardGrid
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import cardGrid from '../components/cardGrid'
 export default {
   name: 'menu-row',
   data () {
@@ -68,6 +41,9 @@ export default {
         hideBtn: 'Hide'
       }
     }
+  },
+  components: {
+    cardGrid
   },
   computed: {
     ...mapState([
@@ -100,51 +76,6 @@ export default {
         }
         return (!this.pinnedCards.length || (this.pinnedCards.length && !this.Cards[idx].active)) ? this.pinCard(pinnedCard)
           : (this.Cards[idx].active) ? this.hidePin(pinnedCard) : null
-      }
-    },
-    handleUpdate (card) {
-      this.switchesActive = !this.switchesActive
-      this.updateData.updating = !this.updateData.updating
-      const { id, updating } = card
-      const updatePayload = {
-        id,
-        updating
-      }
-      this.showUpdateField(updatePayload)
-    },
-    submitUpdate (card) {
-      const cardId = card.id
-      const { title, text, updating } = this.updateData
-      const updateData = {
-        title,
-        text,
-        cardId,
-        updating
-      }
-      this.updateCard(updateData)
-      this.clearForm()
-      this.switchesActive = !this.switchesActive
-    },
-    handleCancel (card) {
-      this.switchesActive = !this.switchesActive
-      this.updateData.updating = !this.updateData.updating
-      const id = card.id
-      const updating = false
-      const blankPayload = {
-        id,
-        updating
-      }
-      this.showUpdateField(blankPayload)
-      this.clearForm()
-    },
-    handleHide (card) {
-      this.hidePin(card)
-    },
-    clearForm () {
-      this.updateData = {
-        title: '',
-        text: '',
-        updating: false
       }
     }
   },
