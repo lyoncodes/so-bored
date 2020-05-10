76cd8636728e41ee75694cab98f8eb7f15912da9
Michael Lyon <lyoncodes@gmail.com>
2020-05-09 19:27:39 -0700

    moves create card formto ruleForm component

---
+++ docs/src/components/ruleForm.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  b-container
+    button(@click="toggleCardForm" class="btn btn-primary") Add New Card
+    b-form(@submit.prevent="handleSubmit" v-if="showCardForm")
+      b-col(md="6")
+        b-form-group(id="input-title" label="Card Title:" label-for="input-title")
+          b-form-input(
+            id="card-title"
+            v-model="formData.title"
+            required
+            placeholder="Enter Rule Title"
+            )
+        b-form-group(id="input-card-text" label="Card Text:" label-for="input-card-text")
+          b-form-input(
+            id="card-text"
+            v-model="formData.text"
+            required
+            placeholder="Enter Rule Text"
+          )
+      b-button(type="submit" variant="primary") Submit
+      b-button(type="reset" variant="danger") Reset
+</template>
+<script>
+import { mapActions, mapState } from 'vuex'
+import ruleForm from '../components/ruleForm'
+
+export default {
+  name: 'Home',
+  components: {
+    ruleForm
+  },
+  data () {
+    return {
+      showCardForm: false,
+      formData: {
+        title: '',
+        text: '',
+        type: '',
+        active: false,
+        updating: false
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      'Menu',
+      'Cards',
+      'allCards'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'addCard'
+    ]),
+    toggleCardForm () {
+      this.showCardForm = !this.showCardForm
+    },
+    handleSubmit () {
+      const { title, text, type, active, updating } = this.formData
+      const id = this.Cards.length
+      const card = {
+        title,
+        text,
+        type,
+        id,
+        active,
+        updating
+      }
+      this.addCard(card)
+      this.formData = {
+        title: '',
+        text: '',
+        type: ''
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- docs/src/views/Home.vue
+++ docs/src/views/Home.vue
@@ -2,75 +2,16 @@
   .home
     img(src="../assets/unearth-slate.svg")
     h1.heading Documents
-    button(@click="toggleCardForm" class="btn btn-primary") Add New Card
-    b-form(@submit.prevent="handleSubmit" v-if="showCardForm")
-      b-col(md="6")
-        b-form-group(id="input-title" label="Card Title:" label-for="input-title")
-          b-form-input(
-            id="card-title"
-            v-model="formData.title"
-            required
-            placeholder="Enter Rule Title"
-            )
-        b-form-group(id="input-card-text" label="Card Text:" label-for="input-card-text")
-          b-form-input(
-            id="card-text"
-            v-model="formData.text"
-            required
-            placeholder="Enter Rule Text"
-          )
-      b-button(type="submit" variant="primary") Submit
-      b-button(type="reset" variant="danger") Reset
+    ruleForm
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import ruleForm from '../components/ruleForm'
+
 export default {
   name: 'Home',
-  data () {
-    return {
-      showCardForm: false,
-      formData: {
-        title: '',
-        text: '',
-        type: '',
-        active: false,
-        updating: false
-      }
-    }
-  },
-  computed: {
-    ...mapState([
-      'Menu',
-      'Cards',
-      'allCards'
-    ])
-  },
-  methods: {
-    ...mapActions([
-      'addCard'
-    ]),
-    toggleCardForm () {
-      this.showCardForm = !this.showCardForm
-    },
-    handleSubmit () {
-      const { title, text, type, active, updating } = this.formData
-      const id = this.Cards.length
-      const card = {
-        title,
-        text,
-        type,
-        id,
-        active,
-        updating
-      }
-      this.addCard(card)
-      this.formData = {
-        title: '',
-        text: '',
-        type: ''
-      }
-    }
+  components: {
+    ruleForm
   }
 }
 </script>
