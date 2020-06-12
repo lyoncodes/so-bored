8a1fae6d31a8699396edc74ba77ba70c91a99433
Michael Lyon <lyoncodes@gmail.com>
2020-06-12 11:54:00 -0700

    adds filter switches by type prototype, renames some functions

---
+++ docs/src/components/ruleFilter.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  b-container
+    b-form-group(id="rule-filter")
+      b-button(
+        id="filter-button"
+        v-for="(select, idx) in Menu"
+        :key="idx"
+        @click="handleFilter(select.type)"
+        variant="light"
+      ) {{ select.type }}
+</template>
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState([
+      'Cards',
+      'Menu',
+      'pinnedCards'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'filterAction'
+    ]),
+    handleFilter (id) {
+      console.log(id)
+      this.filterAction(id)
+    }
+  },
+  mounted () {
+    const menu = this.Menu
+    this.menu = menu
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
