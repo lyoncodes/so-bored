4abc9f2a560b2c5e33316b4f870298d8a5623c7c
Michael Lyon <lyoncodes@gmail.com>
2020-08-03 15:05:20 -0700

    refactors UI functions

--- docs/src/store/mutations.js
+++ docs/src/store/mutations.js
@@ -1,4 +1,31 @@
 export default {
+  // updateState function handles all dispatched actions
+  updateState (state, data) {
+    const ruleEl = state.rules.find(el => el.id === data.id)
+    if (data.payload === 'addRule') {
+      state.rules.push(data)
+    } else if (data.payload === 'deleteRule') {
+      const filteredArr = state.rules.filter((el) => {
+        return (el.id !== data.id) ? el : null
+      })
+      state.rules = filteredArr
+    } else if (data.payload === 'updateRule') {
+      ruleEl.title = data.title
+      ruleEl.text = data.text
+      ruleEl.updating = !data.updating
+    } else if (data.payload === 'toggleUpdateFields') {
+      ruleEl.updating = !data.updating
+    } else if (data.payload === 'addAnnotation') {
+      ruleEl.annotations.push(data)
+    } else if (data.payload === 'deleteAnnotation') {
+      const filteredArr = ruleEl.annotations.filter(el => {
+        return (el.text !== data.text) ? el : null
+      })
+      ruleEl.annotations = filteredArr
+    } else if (data.payload === 'addLink') {
+      ruleEl.links.push(data)
+    }
+  },
   // update userProfile in the state to the user passed on login
   setUserProfile (state, val) {
     state.userProfile = val
@@ -6,75 +33,5 @@ export default {
   // set rules in state to the rules retrieved from db
   setRuleCards (state, rulePayload) {
     state.rules = rulePayload
-  },
-  // adds a rule to database
-  async addRule (state, card) {
-    // checks if rule title already exists
-    const arrCopy = state.rules.filter(el => {
-      return (el.title === card.title) ? el : null
-    })
-    if (!arrCopy.length) {
-      // adds card to rules in state
-      state.rules.push(card)
-    } else alert('this title already exists! Try another entry')
-  },
-  // deactivates card in Cards and pinnedCards arrays
-  removeCard: (state, card) => {
-    const shallowArr = state.rules.filter((el) => {
-      return (el.id !== card.id) ? el : null
-    })
-    state.rules = shallowArr
-  },
-  // change state of cards to updating (disables buttons when card is being edited)
-  updateCardField: (state, card) => {
-    const arr = state.rules
-    arr.map(el => {
-      if (el.id === card.id) {
-        el.updating = !el.updating
-      }
-    })
-  },
-  // update card in cards and pinnedcards arrays & change state of cards to !updating
-  replaceCardRule (state, card) {
-    state.rules.map((el) => {
-      if (el.id === card.id) {
-        el.title = card.title
-        el.text = card.text
-        el.updating = !card.updating
-      }
-    })
-  },
-  // annotates card
-  submitAnnotation: (state, card) => {
-    state.rules.map(el => {
-      if (el.id === card.id) {
-        el.annotations.push(card)
-      }
-    })
-  },
-  removeAnnotation: (state, card) => {
-    state.rules.map(el => {
-      if (el.id === card.id) {
-        const filteredAnnotations = el.annotations.filter(el => {
-          return (el.text !== card.text) ? el : null
-        })
-        el.annotations = filteredAnnotations
-      }
-    })
-  },
-  // adds LinkBox
-  addLink: (state, link) => {
-    state.rules.map(el => {
-      if (el.id === link.id) {
-        el.links.push(link)
-      }
-    })
-  },
-  // maps rules in all Rules
-  mapRules: (state, card) => {
-    const val = state.rules.forEach(element => {
-      return element.id === card.id ? element : null
-    })
-    return val
   }
 }
