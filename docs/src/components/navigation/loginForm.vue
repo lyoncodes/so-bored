265373e52cbd99aca3ab6303f98f19c5a6ad04a0
Michael Lyon <lyoncodes@gmail.com>
2020-11-26 13:11:29 -0800

    refactors login form to its own component

---
+++ docs/src/components/navigation/loginForm.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  b-form(
+    @submit.prevent="handleLogin(loginData)"
+  )
+    .p-2
+      b-col.mt-4
+        img.form-header-icon.mb-4(src='../../assets/doDoA.svg')
+        b-form-group#login-email
+          b-row
+            a.validation-char email
+          b-form-input(
+            v-model="loginData.email"
+            type="text"
+          )
+        b-form-group#login-password
+          b-row
+            a.validation-char password
+          b-form-input(
+            v-model="loginData.password"
+            type="password"
+          )
+        button.neu-c-button.mt-3(type="submit") Log In
+        b-row.justify-content-center.pt-3
+          span New user?
+          a.pl-1(
+            @click="toggleSignUp"
+          ) Sign Up
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      loginData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'login'
+    ]),
+    handleLogin (loginData) {
+      this.login(loginData)
+    },
+    toggleSignUp () {
+      this.$emit('toggleBoolean')
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
--- docs/src/views/Login.vue
+++ docs/src/views/Login.vue
@@ -2,38 +2,14 @@
   b-container
     b-row.justify-content-center.pt-5
       b-col.col-sm-8.col-lg-4(v-if="showLoginForm")
-        b-form(
-          @submit.prevent="handleLogin(loginData)"
-        )
-          .p-2
-            b-col.mt-4
-              img.form-header-icon.mb-4(src='../assets/doDoA.svg')
-              b-form-group#login-email
-                b-row
-                  a.validation-char email
-                b-form-input(
-                  v-model="loginData.email"
-                  type="text"
-                )
-              b-form-group#login-password
-                b-row
-                  a.validation-char password
-                b-form-input(
-                  v-model="loginData.password"
-                  type="password"
-                )
-              button.neu-c-button.mt-3(type="submit") Log In
-              b-row.justify-content-center.pt-3
-                span New user?
-                a.pl-1(
-                  @click="toggleSignUp"
-                ) Sign Up
+        loginForm(v-on:toggleBoolean="toggleSignUp")
       b-col.col-md-8.col-lg-4(v-if="!showLoginForm")
         signUpForm(v-on:toggle="toggleSignUp")
 </template>
 <script>
 import { mapActions } from 'vuex'
 import signUpForm from '../components/navigation/signUpForm'
+import loginForm from '../components/navigation/loginForm'
 export default {
   data () {
     return {
@@ -45,7 +21,8 @@ export default {
     }
   },
   components: {
-    signUpForm
+    signUpForm,
+    loginForm
   },
   methods: {
     ...mapActions([
