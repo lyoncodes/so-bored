8666838114d4996153bbad3739e8a61ce7e41832
Michael Lyon <lyoncodes@gmail.com>
2021-02-17 15:37:03 -0800

    adds delete icon

---
+++ docs/src/components/IconBase.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 15 15"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title
+      :id="iconName"
+      lang="en"
+    >{{ iconName }} icon</title>
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 15
+    },
+    height: {
+      type: [Number, String],
+      default: 15
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
--- docs/src/components/post/comments/postComment.vue
+++ docs/src/components/post/comments/postComment.vue
@@ -19,7 +19,7 @@
         )
 
         //- Array Iteration (comment in postComments prop, passed from postsHome parent component)
-        b-col.col-12.mb-4(v-for="comment in postComments" :key="comment.id").comments-section
+        b-col.col-12.mb-2.mt-2(v-for="comment in postComments" :key="comment.id").comments-section
 
           //- post comment
           b-row.comment-container.text-left
@@ -30,7 +30,11 @@
                 @click="deleteComment(comment)"
                 v-if="user===comment.userName"
               )
-                img#post-nav-icon(v-bind:src="imgStore[5]" width="640" height="360")
+                IconBase(
+                  icon-name="caret"
+                  iconColor="rgba(221, 62, 230, 0.5)"
+                )
+                  IconDelete
 
 </template>
 <script>
@@ -61,7 +65,9 @@ export default {
     }
   },
   components: {
-    createComment
+    createComment,
+    IconBase: () => import('../../IconBase'),
+    IconDelete: () => import('../../icons/IconDelete')
   },
   async mounted () {
     const user = this.user
